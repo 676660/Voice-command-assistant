b5d562a2e4c24157f7c8b596c753c56a9feb1f06
Mathias Kaldheim <676621@stud.hvl.no>
2026-03-12 12:10:45 +0100

    g

--- voiceCommandAssistantCode.py
+++ voiceCommandAssistantCode.py
@@ -2,3 +2,12 @@ import os
 import keras
 import tensorflow as tf
 import tensorflow_datasets as tfds
+
+(train_ds, val_ds, test_ds), ds_info = tfds.load(
+    "speech_commands",
+    split=["train", "validation", "test"],
+    as_supervised=True,
+    with_info=True,
+)
+
+print(ds_info)
